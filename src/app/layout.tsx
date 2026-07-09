55141ba691c02f8494d9e550900861fbe5be491b
Personal <sk7627577@gmail.com>
2026-07-09 14:01:57 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,48 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${firaSans.variable} h-full antialiased`}>
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D52BPEV9BS"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D52BPEV9BS');`}
+        </Script>
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5Z7WM2BP');`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+      </head>
+
       <body suppressHydrationWarning={true}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5Z7WM2BP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
 
         <LandingFooter />
