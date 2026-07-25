8306dc0ed9a29686a42145c4a66b93b14a62cb3f
abhijeet <abhijeetsingh5631@gmail.com>
2026-07-26 00:09:30 +0530

    update

--- src/app/(website)/articles/[slug]/page.tsx
+++ src/app/(website)/articles/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from "next";
-import { SectionWithContainer } from "@/components/sectionComponants";
+import { Section, SectionWithContainer } from "@/components/sectionComponants";
 import { notFound } from "next/navigation";
 import blogPostPageData from "./pageData";
 import ImageBanner from "@/components/banners/ImageBanner";
+import Image from "next/image";
 interface Params {
   params: Promise<{ slug: string }>;
 }
@@ -57,19 +58,26 @@ export async function generateMetadata({ params }: Params): Promise<Metadata> {
 export default async function Page({ params }: Params) {
   const path = await params;
   const pageData = blogPostPageData.find((post) => post.slug === path.slug);
+
+  console.log(pageData)
   if (!pageData) return notFound();
   return (
     <main className="">
-      <ImageBanner
+      {/* <ImageBanner
         image={pageData?.bannerImage as string}
         title={pageData?.title as string}
         tag=""
         description=""
         benefits=""
-      />
+      /> */}
+
+
+      <div className="relative max-w-6xl mx-auto  w-full lg:aspect-3/2 aspect-[4/5.2] overflow-hidden">
+        <Image src={pageData?.bannerImage} alt={pageData?.title} fill className="object-cover" />
+      </div>
       <SectionWithContainer>
         <div
-          className="blog-data"
+          className="max-w-6xl mx-auto blog-content!"
           dangerouslySetInnerHTML={{ __html: pageData?.content || "" }}
         />
       </SectionWithContainer>
